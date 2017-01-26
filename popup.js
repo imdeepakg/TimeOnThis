d1ee9ffb53321dcf9d13e273a11d10d212b3d718
imdeepakg <deepakgupta101@yahoo.co.in>
2017-01-26 14:24:37 +0530

    Time Format changes

--- popup.js
+++ popup.js
@@ -11,7 +11,10 @@ function updateStatus()
 }
 
 function createTimeMessage(seconds) {
-	if(!seconds) {
+	//0 is a falsey value
+	//http://stackoverflow.com/questions/7615214/in-javascript-why-is-0-equal-to-false-but-when-tested-by-if-it-is-not-fals
+	//http://stackoverflow.com/questions/5515310/is-there-a-standard-function-to-check-for-null-undefined-or-blank-variables-in
+	if(seconds === undefined || seconds === null) {
 		return "Not Available";
 	}
 	var min = Math.floor((seconds / 60) % 60);
@@ -26,17 +29,27 @@ function createTimeMessage(seconds) {
 	{
 		msg = day + " Day";
 	}
-	else if(hour > 0)
-	{
-		msg = hour + " Hour";
-	}
-	else if(min > 0)
-	{
-		msg = min + " Minute";
-	}
-	else
-	{
-		msg = sec + " Second";
+	else {
+		if(hour > 9) {
+			msg = hour + ":";
+		}
+		else {
+			msg = "0" + hour + ":";
+		}
+		
+		if(min > 9) {
+			msg += min + ":";
+		}
+		else {
+			msg += "0" + min + ":";
+		}
+
+		if(sec > 9) {
+			msg += sec;
+		}
+		else {
+			msg += "0" + sec;
+		}
 	}
 	
 	return msg;
